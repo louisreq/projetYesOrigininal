93384f8d6a1142bc00c315f3b2e98e25ab9fd65f
Bendrog <32641320+Bendrog@users.noreply.github.com>
2018-03-08 14:31:43 +0100

    dbCredentials

--- src/main/java/hei/devweb/traderz/dao/DataSourceProvider.java
+++ src/main/java/hei/devweb/traderz/dao/DataSourceProvider.java
@@ -13,11 +13,11 @@ public class DataSourceProvider {
     public static DataSource getDataSource() {
         if (dataSource == null) {
             dataSource = new MysqlDataSource();
-            dataSource.setServerName("localhost");
+            dataSource.setServerName("r42ii9gualwp7i1y.chr7pe7iynqr.eu-west-1.rds.amazonaws.com");
             dataSource.setPort(3306);
-            dataSource.setDatabaseName("traderz");
-            dataSource.setUser("root");
-            dataSource.setPassword("root");
+            dataSource.setDatabaseName("mjcm9ja854h198zx");
+            dataSource.setUser("x05hr5rbcfr652gq");
+            dataSource.setPassword("vjlgs8miv1jqwhyy");
         }
         return dataSource;
     }
